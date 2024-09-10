3f8880c2669d384139d726857f8f4d094a353181
kamo-eri <ekamo.tech@gmail.com>
2024-09-10 16:02:38 +0900

    first commit

---
+++ src/main/java/com/example/pictgram/TbPictgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbPictgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbPictgramApplication.class, args);
+	}
+
+}
