033fceae7a3d7723304c4c3e67d156f323d3227b
kamo-eri <ekamo.tech@gmail.com>
2024-10-09 23:26:37 +0900

    36. SNS 連携 #13

--- src/main/java/com/example/pictgram/TbPictgramApplication.java
+++ src/main/java/com/example/pictgram/TbPictgramApplication.java
@@ -4,9 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class TbPictgramApplication extends SpringBootServletInitializer {
+    
+    @Bean
+    public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                clients, authz);
+        return WebClient.builder().filter(oauth2).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TbPictgramApplication.class, args);
